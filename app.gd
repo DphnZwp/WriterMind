56b40b357ee7683c541e2f2ffdbd9e786b09e98d
DphnZwp <zwuupdaphne@gmail.com>
2024-07-20 19:49:16 +0200

    latest changes

--- app.gd
+++ app.gd
@@ -7,13 +7,27 @@ func _process(delta):
 	pass
 
 func _on_open_file_pressed():
+	$FileDialog.access = FileDialog.ACCESS_FILESYSTEM
+	$FileDialog.current_dir = OS.get_environment("USERPROFILE") + "/Downloads"
 	$FileDialog.popup()
 
 func _on_save_file_pressed():
+	$SaveDialog.access = FileDialog.ACCESS_FILESYSTEM
+	$SaveDialog.current_dir = OS.get_environment("USERPROFILE") + "/Downloads"
 	$SaveDialog.popup()
 
 func _on_file_dialog_file_selected(path):
 	var file = FileAccess.open(path, FileAccess.READ)
 	var content = file.get_as_text()
-	file.close()  # Don't forget to close the file when you're done
-	$TextEdit.text = content  
+	$TextEdit.text = content
+	file.close()
+
+
+func _on_save_dialog_file_selected(path):
+	var file = FileAccess.open(path, FileAccess.WRITE)
+	file.store_string($TextEdit.text)
+	file.close()
+
+
+func _on_new_file_pressed():
+	pass # Replace with function body.
